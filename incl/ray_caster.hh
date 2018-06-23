14b95aed11c8f35a31acf5a996f16a42593e47b9
dmraji <davidmraji@gmail.com>
2018-06-22 17:47:18 -0700

    Updates

--- incl/ray_caster.hh
+++ incl/ray_caster.hh
@@ -21,7 +21,18 @@ class ray_caster
                   int**
                   );
 
+    std::vector<float> linspace(float,
+                                float,
+                                int
+                                );
+
   private:
+    float diff_x, diff_y, diff_z;
+    float min_x, min_y, min_z;
+    float max_x, max_y, max_z;
+    int x_pts, y_pts, z_pts, tot_pts;
+    int box_ind;
+    std::vector<float> spaced_x, spaced_y, spaced_z;
 
 
 };
--- src/ray_caster.cc
+++ src/ray_caster.cc
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
+
 ray_caster::ray_caster(float **ptcld,
                        int ptcld_len,
                        float **path,
@@ -15,16 +17,60 @@ ray_caster::ray_caster(float **ptcld,
                        )
 {
 
-  std::cout << boundaries[0][0] - boundaries[1][0] << '\n';
+  diff_x = abs(boundaries[0][0] - boundaries[1][0]);
+  diff_y = abs(boundaries[0][1] - boundaries[1][1]);
+  diff_z = abs(boundaries[0][2] - boundaries[1][2]);
 
-  // float** box = new float*[]
+  std::cout << "diff_x: " << diff_x << '\n';
+  std::cout << "diff_y: " << diff_y << '\n';
+  std::cout << "diff_z: " << diff_z << '\n';
 
+  min_x = boundaries[0][0];
+  min_y = boundaries[0][1];
+  min_z = boundaries[0][2];
+  max_x = boundaries[1][0];
+  max_y = boundaries[1][1];
+  max_z = boundaries[1][2];
+
+  x_pts = 200;
+  y_pts = 200;
+  z_pts = 20;
+  tot_pts = x_pts*y_pts*z_pts;
+
+  float** box = new float*[tot_pts];
+  for(int i = 0; i < tot_pts; i++)
+  {
+    box[i] = new float[3];
+  }
+
+  spaced_x = linspace(min_x, max_x, x_pts);
+  spaced_y = linspace(min_y, max_y, y_pts);
+  spaced_z = linspace(min_z, max_z, z_pts);
+
+  box_ind = 0;
+  for(int i = 0; i < x_pts; i++)
+  {
+    for(int j = 0; j < y_pts; j++)
+    {
+      for(int k = 0; k < z_pts; k++)
+      {
+        box[box_ind][0] = spaced_x[i];
+        box[box_ind][1] = spaced_y[j];
+        box[box_ind][2] = spaced_z[k];
+
+        ++box_ind;
+      }
+    }
+  }
+
+  // Initialize hit tracker for pointcloud
   int** ptcld_hit = new int*[ptcld_len];
   for(int i = 0; i < ptcld_len; i++)
   {
     ptcld_hit[i] = new int[3];
   }
 
+  // Initialize miss tracker for pointcloud
   int** ptcld_miss = new int*[ptcld_len];
   for(int i = 0; i < ptcld_len; i++)
   {
@@ -34,12 +80,13 @@ ray_caster::ray_caster(float **ptcld,
   for(int o = 0; o < 1; o++)
   {
 
-    std::cout << "hi" << '\n';
+
 
     // for(int p = 0; p < ptcld_len; p++)
     // {
     //
     // }
+
   }
 
   int_kill(ptcld_len,
@@ -50,11 +97,13 @@ ray_caster::ray_caster(float **ptcld,
            ptcld_miss
            );
 
-  std::cout << "hil" << '\n';
-
 }
 
+//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
+
 ray_caster::~ray_caster() {}
+
+//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
 
 // Free heap space
 void ray_caster::int_kill(int pos_len,
@@ -67,4 +116,29 @@ void ray_caster::int_kill(int pos_len,
   }
 
   delete [] pos;
+}
+
+//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
+
+// Linspace-like vectors
+vector<float> ray_caster::linspace(float a, float b, int n)
+{
+    vector<float> spaced_vec;
+    float step = (b-a) / (n-1);
+
+    while(a <= b)
+    {
+        array.push_back(a);
+        a += step;
+    }
+
+    return spaced_vec;
+}
+
+//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
+
+float ray_caster::bound_build(float **boundaries
+                              )
+{
+
 }

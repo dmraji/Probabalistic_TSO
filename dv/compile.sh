352f7d1abfb70f27ec8b1a3d3623e71c1230fb25
dmraji <davidmraji@gmail.com>
2018-07-16 11:36:51 -0700

    Reorg

--- dv/compile.sh
+++
@@ -1,7 +0,0 @@
-echo "Removing old build"
-rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt Makefile binning
-
-echo "Building..."
-cmake -DCMAKE_BUILD_TYPE=Release .
-
-echo "Done! Now run $ make to compile."

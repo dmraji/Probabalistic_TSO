c30235f3627ee829d55cecb495ed1b15017f7434
dmraji <davidmraji@gmail.com>
2018-07-16 10:13:27 -0700

    Memory optimization and guarding in raycasting; minor reorg

--- dv/binning.cc
+++ dv/binning.cc
@@ -4,6 +4,7 @@
 #include <sstream>
 
 #include <string>
+#include <cstring>
 
 #include <vector>
 #include <algorithm>
@@ -89,6 +90,49 @@ float sq(float n
 
 //_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
 
+// Implementation of CUDA functions to reinterpret casts
+float __int_as_float(int32_t a)
+{
+  float r;
+  memcpy(&r, &a, sizeof(r));
+  return r;
+}
+
+int32_t __float_as_int(float a)
+{
+  int32_t r;
+  memcpy(&r, &a, sizeof(r));
+  return r;
+}
+
+//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
+
+// Superior natural logarithm
+float f_logf (float a)
+{
+  float m, r, s, t, i, f;
+  int32_t e;
+
+  e = (__float_as_int(a) - 0x3f2aaaab) & 0xff800000;
+  m = __int_as_float(__float_as_int(a) - e);
+  i = (float)e * 1.19209290e-7f; // 0x1.0p-23
+
+  /* m in [2/3, 4/3] */
+  f = m - 1.0f;
+  s = f * f;
+
+  /* Compute log1p(f) for f in [-1/3, 1/3] */
+  r = fmaf(0.230836749f, f, -0.279208571f); // 0x1.d8c0f0p-3, -0x1.1de8dap-2
+  t = fmaf(0.331826031f, f, -0.498910338f); // 0x1.53ca34p-2, -0x1.fee25ap-2
+  r = fmaf(r, s, t);
+  r = fmaf(r, s, f);
+  r = fmaf(i, 0.693147182f, r); // 0x1.62e430p-1 // log(2)
+
+  return r;
+}
+
+//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
+
 // Raycasting function; uses discretization to acquire points along ray path
 void cast_ray(pt &origin,
               pt &end,
@@ -100,14 +144,19 @@ void cast_ray(pt &origin,
   float mag = std::sqrt(sq(dist.x) + sq(dist.y) + sq(dist.z));
 
   // Floor instead of ceil to dampen chance of mistaken marking of endpt as free
-  int disc = int(std::floor(mag/resolution));
+  int disc = int(std::ceil(mag/resolution));
   pt inc = {dist.x / disc, dist.y / disc, dist.z / disc};
 
+  // Warn on short ray
+  if(disc < 1)
+  {
+    std::cout << "WARNING: Ray of length less than 1!" << endl;
+  }
   // Do not mark endpt as free
   ray.reserve(disc-1);
   for(int i = 0; i < disc-1; ++i)
   {
-    ray.push_back({origin.x + inc.x * i, origin.y + inc.y * i, origin.z + inc.z});
+    ray.push_back({origin.x + inc.x * i, origin.y + inc.y * i, origin.z + inc.z * i});
   }
 
 }
@@ -136,6 +185,7 @@ namespace std
 
 int main(int argc, char **argv)
 {
+
   std::clock_t start;
   start = std::clock();
 
@@ -159,7 +209,7 @@ int main(int argc, char **argv)
 
   unordered_map <ind, int> box_free;
   unordered_map <ind, int> box_occ;
-  unordered_map <ind, int> box_occ_prob;
+  unordered_map <ind, float> box_occ_prob;
   // boost::unordered_multimap<ind, pt, boost::hash<ind>> box_free;
 
   // pt origin = {1.053, -4.234, 0.123};
@@ -167,7 +217,7 @@ int main(int argc, char **argv)
 
   float resolution = 0.1;
 
-  for(int path_ind = 0; path_ind < 100; ++path_ind)
+  for(int path_ind = 0; path_ind < path_len; ++path_ind)
   {
 
     pt origin = {path[path_ind][0], path[path_ind][1], path[path_ind][2]};
@@ -190,6 +240,9 @@ int main(int argc, char **argv)
 
     cloud_cut = cloud_cut + cloud_chunk_len;
 
+
+    std::vector<pt> empty_vec;
+
     for(int cld_ind = 0; cld_ind < cloud_chunk_len; ++cld_ind)
     {
 
@@ -211,31 +264,35 @@ int main(int argc, char **argv)
         ++box_free[ {x_ind, y_ind, z_ind} ];
       }
 
+      ray.swap(empty_vec);
+
       // Mark occupied space
       int x_ind = (end.x * (1/resolution));
       int y_ind = (end.y * (1/resolution));
       int z_ind = (end.z * (1/resolution));
       ++box_occ[ {x_ind, y_ind, z_ind} ];
 
+      // Update occupnacy probability
+      box_occ_prob[ {x_ind, y_ind, z_ind} ] = f_logf(box_occ[ {x_ind, y_ind, z_ind} ] / (path_ind + 1));
+
     }
 
     timestamp(start,
               std::to_string(path_ind));
 
-    // Probabalistic occupancy
-
-
   }
 
-  // unordered_multimap <ind, pt, hasher, key_equal> :: iterator it;
+  unordered_multimap <ind, float> :: iterator it;
 
-  // cout << "Unordered multimap contains: " << endl;
-  // for (it = box_free.begin(); it != box_free.end(); ++it)
-  // {
-  //
-  //   std::cout << "(" << it->first.x << ", " << it->first.y << ", " << it->first.z << " : "
-  //             << it->second.x << ", " << it->second.y << ", " << it->second.z << ")" << endl;
-  // }
+  cout << "Unordered multimap contains: " << endl;
+  for (it = box_occ_prob.begin(); it != box_occ_prob.end(); ++it)
+  {
+
+    std::cout << "(" << it->first.x << ", " << it->first.y << ", " << it->first.z << " : "
+              << it->second << ")" << endl;
+  }
+
+  std::cout << box_occ_prob.size() << '\n';
 
   timestamp(start,
             "end"

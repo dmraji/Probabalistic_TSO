f4da30aa580f73d25174056c3f480e4edfef480f
David <davidmraji@gmail.com>
2018-07-24 22:00:21 -0700

    H5 posdata

--- incl/pt.hh
+++ incl/pt.hh
@@ -8,4 +8,16 @@ struct pt
   const float x, y, z;
 };
 
+// To read from hdf5
+struct pt_a
+{
+  float x, y, z;
+};
+
+// Rotation matrix
+struct rot_mat
+{
+  float r00, r01, r02, r10, r11, r12, r20, r21, r22;
+};
+
 #endif
--- rc.cc
+++ rc.cc
@@ -85,113 +85,67 @@ void h5_read(std::string file_name,
              std::string dataset_name
              )
 {
-  namespace H5
-  {
-    const H5std_string FILE_NAME( file_name );
-    const H5std_string DATASET_NAME( dataset_name );
-    const int    NX_SUB = 3;    // hyperslab dimensions
-    const int    NY_SUB = 4;
-    const int    NX = 7;        // output buffer dimensions
-    const int    NY = 7;
-    const int    NZ = 3;
-    const int    RANK_OUT = 3;
+  using namespace H5;
 
-    try
-    {
-      /*
-      * Turn off the auto-printing when failure occurs so that we can
-      * handle the errors appropriately
-      */
-      Exception::dontPrint();
-      /*
-      * Open the specified file and the specified dataset in the file.
-      */
-      H5File file( FILE_NAME, H5F_ACC_RDONLY );
-      DataSet dataset = file.openDataSet( DATASET_NAME );
+  const H5std_string FILE_NAME( file_name );
+  const H5std_string DATASET_NAME( dataset_name );
 
-      DataSpace dataspace = dataset.getSpace();
-      /*
-      * Get the number of dimensions in the dataspace.
-      */
-      int rank = dataspace.getSimpleExtentNdims();
-      /*
-      * Get the dimension size of each dimension in the dataspace and
-      * display them.
-      */
-      hsize_t dims_out[2];
-      int ndims = dataspace.getSimpleExtentDims( dims_out, NULL);
-      cout << "rank " << rank << ", dimensions " << (unsigned long)(dims_out[0]) << " x " <<(unsigned long)(dims_out[1]) << endl;
-      /*
-      * Define hyperslab in the dataset; implicitly giving strike and
-      * block NULL.
-      */
-      hsize_t offset[2];   // hyperslab offset in the file
-      hsize_t count[2];    // size of the hyperslab in the file
-      offset[0] = 1;
-      offset[1] = 2;
-      count[0] = NX_SUB;
-      count[1] = NY_SUB;
-      dataspace.selectHyperslab( H5S_SELECT_SET, count, offset );
-      /*
-      * Define the memory dataspace.
-      */
-      hsize_t dimsm[3]; /* memory space dimensions */
-      dimsm[0] = NX;
-      dimsm[1] = NY;
-      dimsm[2] = NZ ;
-      DataSpace memspace( RANK_OUT, dimsm );
-      /*
-      * Define memory hyperslab.
-      */
-      hsize_t offset_out[3]; // hyperslab offset in memory
-      hsize_t count_out[3]; // size of the hyperslab in memory
-      offset_out[0] = 3;
-      offset_out[1] = 0;
-      offset_out[2] = 0;
-      count_out[0] = NX_SUB;
-      count_out[1] = NY_SUB;
-      count_out[2] = 1;
-      memspace.selectHyperslab( H5S_SELECT_SET, count_out, offset_out );
-      /*
-      * Read data from hyperslab in the file into the hyperslab in
-      * memory and display the data.
-      */
-      dataset.read( data_out, PredType::NATIVE_INT, memspace, dataspace );
-      for (j = 0; j < NX; j++)
-      {
-        for (i = 0; i < NY; i++)
-        {
-          cout << data_out[j][i][0] << " ";
-        }
-      cout << endl;
-      }
-    }  // end of try block
+  H5File file( FILE_NAME, H5F_ACC_RDONLY );
+  DataSet dataset = file.openDataSet( DATASET_NAME );
 
-    // catch failure caused by the H5File operations
-    catch( FileIException error )
-    {
-       error.printError();
-       return -1;
-    }
-    // catch failure caused by the DataSet operations
-    catch( DataSetIException error )
-    {
-       error.printError();
-       return -1;
-    }
-    // catch failure caused by the DataSpace operations
-    catch( DataSpaceIException error )
-    {
-       error.printError();
-       return -1;
-    }
-    // catch failure caused by the DataSpace operations
-    catch( DataTypeIException error )
-    {
-       error.printError();
-       return -1;
-    }
-  }
+  const H5std_string MEMBER_X("x");
+  const H5std_string MEMBER_Y("y");
+  const H5std_string MEMBER_Z("z");
+  // const H5std_string MEMBER_INTENSITY("intensity_name");
+  // const H5std_string MEMBER_RING("ring_name");
+
+  CompType h5_pt_type( sizeof(pt_a) );
+  h5_pt_type.insertMember(MEMBER_X, 0, PredType::NATIVE_FLOAT);
+  h5_pt_type.insertMember(MEMBER_Y, sizeof(float), PredType::NATIVE_FLOAT);
+  h5_pt_type.insertMember(MEMBER_Z, sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+
+  int cld_pts = 40987774;
+  pt_a *data_h5 = new pt_a[cld_pts];
+  memset(data_h5, 0, cld_pts);
+  dataset.read(data_h5, h5_pt_type);
+
+  // for(int i = 0; i < 10000; ++i)
+  // {
+  //   std::cout << i << ": " << data_h5[i].x << '\n';
+  // }
+
+  const H5std_string DATASET_NAME( "/posData" );
+  DataSet dataset = file.openDataSet( DATASET_NAME );
+
+  const H5std_string MEMBER_TX("tx");
+  const H5std_string MEMBER_TY("ty");
+  const H5std_string MEMBER_TZ("tz");
+
+  const H5std_string MEMBER_R00("ROO");
+  const H5std_string MEMBER_R01("R01");
+  const H5std_string MEMBER_R02("R02");
+  const H5std_string MEMBER_R10("R10");
+  const H5std_string MEMBER_R11("R11");
+  const H5std_string MEMBER_R12("R12");
+  const H5std_string MEMBER_R20("R20");
+  const H5std_string MEMBER_R21("R21");
+  const H5std_string MEMBER_R22("R22");
+
+  ComptType h5_pt_type( sizeof(pt_a) );
+  h5_pt_type.insertMember(MEMBER_TX, 0, PredType::NATIVE_FLOAT);
+  h5_pt_type.insertMember(MEMBER_TY, sizeof(float), PredType::NATIVE_FLOAT);
+  h5_pt_type.insertMember(MEMBER_TZ, sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+
+  CompType h5_rot_type( sizeof(rot_mat) );
+  h5_rot_type.insertMember(MEMBER_R00, 0, PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R01, sizeof(float), PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R02, sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R10, sizeof(float)+sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R11, sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R12, sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R20, sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R21, sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
+  h5_rot_type.insertMember(MEMBER_R22, sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float)+sizeof(float), PredType::NATIVE_FLOAT);
 }
 
 //_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//_//
